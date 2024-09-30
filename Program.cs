d071e513f369adac4d5af2d7d9172547a5e4bb92
Adrozs <adrozsahegyi@gmail.com>
2024-09-30 18:15:19 +0200

    -removed jwt as it is not needed in frontend -added compression logic to optimize load times

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace RestaurantBookingFrontend
 {
     public class Program
@@ -10,21 +15,97 @@ namespace RestaurantBookingFrontend
             builder.Services.AddControllersWithViews();
             builder.Services.AddHttpClient();
 
+            // Add JWT Authentication
+            //builder.Services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //.AddJwtBearer(options =>
+            //{
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuer = true,
+            //        ValidateAudience = true,
+            //        ValidateLifetime = true,
+            //        ValidateIssuerSigningKey = true,
+            //        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            //        ValidAudience = builder.Configuration["Jwt:Audience"],
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]))
+            //    };
+            //});
+
+            // Adding session - used for the IsLoggedIn session
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+
+            // Configure Brotli and Gzip compression
+            builder.Services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+                //options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/svg+xml", "application/javascript", "text/css" }); // new
+
+            });
+
+            builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = System.IO.Compression.CompressionLevel.Fastest;
+            });
+
+            builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = System.IO.Compression.CompressionLevel.Fastest;
+            });
+
+            // Configure static file caching
+            //builder.Services.AddResponseCaching();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable static file caching
+            //app.UseStaticFiles(new StaticFileOptions
+            //{
+            //    OnPrepareResponse = ctx =>
+            //    {
+            //        // If image cache for 1 month
+            //        if (ctx.Context.Request.Path.StartsWithSegments("/Images"))
+            //            ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=2629743");
+            //        // Else caches for 7 days
+            //        else
+            //            ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=604800");
+            //    }
+            //});
+
+
+            // Enable caching
+            //app.UseResponseCaching();
+            
+            // Enable compression 
+            app.UseResponseCompression();
+
+            app.UseSession();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            // Add Authentication Middleware
+            app.UseAuthentication(); // Ensure this is before Authorization
             app.UseAuthorization();
 
             app.MapControllerRoute(
