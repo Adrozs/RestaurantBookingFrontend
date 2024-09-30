10e6fb3d03f74b96623e96bb6d8c6ac84b712856
Adrozs <adrozsahegyi@gmail.com>
2024-09-30 18:19:29 +0200

    Added model for admin login credentials

---
+++ Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantBookingFrontend.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string AdminUsername { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string AdminPassword { get; set; }
+    }
+}
